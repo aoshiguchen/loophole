f2db1cbd0b41623970ee48387de55bf5cc8eeb07
杨文 <wen.yang@eku001.com>
2019-06-02 15:59:32 +0800

    没有配置钉钉token时，使用默认服务发送

--- src/main/java/com/aoshiguchen/loophole/service/DingService.java
+++ src/main/java/com/aoshiguchen/loophole/service/DingService.java
@@ -1,7 +1,9 @@
 package com.aoshiguchen.loophole.service;
 
 import com.aoshiguchen.loophole.base.SystemConfig;
+import com.aoshiguchen.loophole.core.foundation.Foundation;
 import com.aoshiguchen.loophole.core.uitls.DingTalkUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,7 +19,11 @@ public class DingService {
      * @param msg
      */
     public void notice(String msg) {
-        DingTalkUtil.sendDingTalkMsg(SystemConfig.DING_NOTICE_TOKEN,msg);
+        if(StringUtils.isEmpty(SystemConfig.DING_NOTICE_TOKEN)){
+            Foundation.ding().notice(msg);
+        }else{
+            DingTalkUtil.sendDingTalkMsg(SystemConfig.DING_NOTICE_TOKEN,msg);
+        }
     }
 
 }
