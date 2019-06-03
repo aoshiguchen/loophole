d708270bff51b0a8446c8f33a86bb68523019731
杨文 <wen.yang@eku001.com>
2019-06-03 18:10:08 +0800

    更新启动逻辑，启动时，运行所有的扫描任务

--- src/main/java/com/aoshiguchen/loophole/Application.java
+++ src/main/java/com/aoshiguchen/loophole/Application.java
@@ -1,16 +1,17 @@
 package com.aoshiguchen.loophole;
 
-import com.aoshiguchen.loophole.service.RedisLoopholeScanService;
+import com.aoshiguchen.loophole.base.BaseLoopholeScanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
     @Autowired
-    private RedisLoopholeScanService redisLoopholeScanService;
+    private ApplicationContext appContext;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
@@ -18,7 +19,12 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        redisLoopholeScanService.scan();
+        for(String name : appContext.getBeanDefinitionNames()){
+            if(appContext.getBean(name) instanceof BaseLoopholeScanService){
+                BaseLoopholeScanService loopholeScanService = (BaseLoopholeScanService)appContext.getBean(name);
+                loopholeScanService.scan();
+            }
+        }
     }
 
 }
